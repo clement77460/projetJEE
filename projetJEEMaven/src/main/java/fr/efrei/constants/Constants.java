53812c19668d108d41c391e937d54fbf90084e12
clement <clement.jaworski@hotmail.com>
2018-12-01 14:19:37 +0100

    adding maven project
    ToDo: JPA // adding disconnect button in employeView // new dataBase with amazon

---
+++ projetJEEMaven/src/main/java/fr/efrei/constants/Constants.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.efrei.constants;
+
+/**
+ *
+ * @author Clément
+ */
+public final class Constants {
+    
+    
+    public final static String EMPTY_STRING="";
+    
+    public final static String ACTION="action";
+    public final static String ACTION_CHOOSED="actionChoosed";
+    
+    public final static String ACTION_LOGIN="Login";
+    public final static String ACTION_SUPPRIMER="Supprimer";
+    public final static String ACTION_AJOUTER="Ajouter";
+    public final static String ACTION_DETAILS="Details";
+    public final static String ACTION_VALIDER="Valider";
+    public final static String ACTION_GET_LIST="Voir liste";
+    
+    
+    public final static String EMPLOYES="employes";
+    public final static String EMPLOYE="employe";
+    public final static String EMPLOYE_NOM="nom";
+    public final static String EMPLOYE_PRENOM="prenom";
+    public final static String EMPLOYE_TEL_DOM="telDom";
+    public final static String EMPLOYE_TEL_MOB="telMob";
+    public final static String EMPLOYE_TEL_PRO="telPro";
+    public final static String EMPLOYE_ADR="adresse";
+    public final static String EMPLOYE_CP="codePostal";
+    public final static String EMPLOYE_VILLE="ville";
+    public final static String EMPLOYE_MAIL="mail";
+    
+    
+    public final static String USER="user";
+    public final static String PASSWORD="password";
+    
+    public final static String ERROR_MESSAGE="errorMessage";
+    public final static String ERROR_MESSAGE_FILL_ALL="Vous devez renseigner les deux champs";
+    public final static String ERROR_MESSAGE_FAILURE="Echec de la connexion! Vérifiez votre login et/ou mot de passe et essayez à nouveau";
+    
+    public final static String TYPE_MESSAGE="typeMessage";
+    
+    public final static String RADIOS_VALUE="radiosSelected";
+    
+    public final static String DB_URL_PROPERTIES="dbUrl";
+    public final static String DB_USER_PROPERTIES="dbUser";
+    public final static String DB_PASS_PROPERTIES="dbPwd";
+    
+    public final static String DRIVER_MYSQL="com.mysql.jdbc.Driver";
+    
+    
+    
+    
+    
+}
